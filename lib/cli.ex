561be55ccccdbeb0eaf027cb7feaa2cb6271b207
Nicole <pellicena@gmail.com>
2018-10-30 22:05:27 +0000

    Parse arguments from command line

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,5 +1,16 @@
 defmodule BunFight.CLI do
+
   def main(_args) do
     IO.puts("Let's start the Bun Fight!")
+  end
+
+  def parse_args(args) do
+    parse = OptionParser.parse(args, switches: [help: :boolean],
+                                     aliases:  [h: :help])
+
+    case parse do
+      {[ help: true], _, _} -> :help
+      _ -> :help
+    end
   end
 end
--- test/cli_test.exs
+++ test/cli_test.exs
@@ -3,12 +3,18 @@ defmodule BunFight.CLITest do
 
   import ExUnit.CaptureIO
 
-  test "CLI" do
+  test "running the CLI" do
     execute_main = fn ->
       BunFight.CLI.main([])
     end
 
     assert capture_io(execute_main) == "Let's start the Bun Fight!\n"
+  end
+
+  test ":help returned by option parsing with -h and --help or no options" do 
+    assert BunFight.CLI.parse_args(["--help"]) == :help
+    assert BunFight.CLI.parse_args(["--help", "fight"]) == :help
+    assert BunFight.CLI.parse_args([]) == :help
   end
 
 end
